a7281e7d14a65a70aba9c8dca6cdf80076e9da5b
tallbiscuits <112773728+tallbiscuits@users.noreply.github.com>
2022-09-09 20:44:17 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -661,9 +661,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  32.29
-    #define DEFAULT_Ki   3.92
-    #define DEFAULT_Kd  66.51
+    #define DEFAULT_Kp  22.89
+    #define DEFAULT_Ki   1.87
+    #define DEFAULT_Kd  70.18
   #endif
 #endif
 
@@ -746,9 +746,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 40.79
-  #define DEFAULT_bedKi  7.84
-  #define DEFAULT_bedKd 141.42
+  #define DEFAULT_bedKp 462.10
+  #define DEFAULT_bedKi  85.47
+  #define DEFAULT_bedKd 624.59
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
